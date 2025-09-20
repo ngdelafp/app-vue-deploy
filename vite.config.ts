32f1f82dea4ad77d7c2d1028010770ec2de6e04a
ngdelafp <112743930+ngdelafp@users.noreply.github.com>
2025-09-20 16:57:03 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'http://ngdelafp.github.io/app-vue-deploy'
+  base:'https://ngdelafp.github.io/app-vue-deploy'
 })
